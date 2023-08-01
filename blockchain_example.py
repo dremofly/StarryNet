0e8c70ffe7f7faa07083426fc214c3e3dbda58c5
dremofly tang <dremofly@gmail.com>
2023-08-02 00:39:25 +0800

    🚀 ground station区块链使用的测试程序

--- blockchain_example.py
+++ blockchain_example.py
@@ -5,8 +5,9 @@ import pandas as pd
 
 if __name__ == "__main__":
     
-    AS = [[1, 28]]  # Node #1 to Node #27 are within the same AS.
-    GS_lat_long = [[50.110924, 8.682127], [46.635700, 14.311817], [43.185857, 20.9384857]
+    AS = [[1, 29]]  # Node #1 to Node #27 are within the same AS.
+    # TODO 使用更准确的经纬度
+    GS_lat_long = [[50.110924, 8.682127], [46.635700, 14.311817], [52.810924, 10.682127], [43.185857, 20.9384857]
                    ]  # latitude and longitude of frankfurt and  Austria
     configuration_file_path = "./config.json.nuc"
     hello_interval = 1  # hello_interval(s) in OSPF. 1-200 are supported.
